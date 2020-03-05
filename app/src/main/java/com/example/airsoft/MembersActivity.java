310086f0b7f9aafb5eb1236c0bdbba59d070f9f0
3DAPOBA <nebo512@yandex.ru>
2020-03-05 11:34:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/airsoft/MembersActivity.java
@@ -0,0 +1,16 @@
+package com.example.airsoft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MembersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_members);
+    }
+
+
+}
