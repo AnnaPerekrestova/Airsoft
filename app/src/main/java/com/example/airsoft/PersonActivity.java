bbb8a9a3dcb91243dd41987fb107f3379913b90b
AnnaPerekrestova <anna.perekrestova111@gmail.com>
2020-04-21 21:53:35 +0300

    new push with db

--- app/src/main/java/com/example/airsoft/PersonActivity.java
+++ app/src/main/java/com/example/airsoft/PersonActivity.java
@@ -14,6 +14,8 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import org.w3c.dom.Text;
 
+import static java.util.logging.Logger.global;
+
 public class PersonActivity extends AppCompatActivity {
 
     @Override
@@ -22,14 +24,39 @@ public class PersonActivity extends AppCompatActivity {
         setContentView(R.layout.activity_person);
         addListenerOnButton();
     }
-
-    public void addListenerOnButton() {
-        Button buttonPersonSave = findViewById(R.id.person_save);
-        Button buttonPersonCancel = findViewById(R.id.person_cancel);
+    //String fio,String nickname,String Position, String arsenal
+    public void to_db(){
         final String personFIO = ((EditText)findViewById(R.id.person_fio)).getText().toString();
         final String personNickname = ((EditText)findViewById(R.id.nickname)).getText().toString();
         final String personPosition = ((EditText)findViewById(R.id.person_position)).getText().toString();
         final String personArsenal = ((EditText)findViewById(R.id.arsenal)).getText().toString();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference db_personFIO;
+        DatabaseReference db_personNickname;
+        DatabaseReference db_personPosition;
+        DatabaseReference db_personArsenal;
+        db_personFIO = database.getReference("Team/members_id/1/FIO");
+        db_personNickname= database.getReference("Team/members_id/1/Nickname");
+        db_personPosition = database.getReference("Team/members_id/1/Position");
+        db_personArsenal = database.getReference("Team/members_id/1/Arsenal");
+        db_personFIO = database.getReference("Team/members_id/1/FIO");
+        db_personNickname= database.getReference("Team/members_id/1/Nickname");
+        db_personPosition = database.getReference("Team/members_id/1/Position");
+        db_personArsenal = database.getReference("Team/members_id/1/Arsenal");
+
+        db_personFIO.setValue(personFIO);
+        db_personNickname.setValue(personNickname);
+        db_personPosition.setValue(personPosition);
+        db_personArsenal.setValue(personArsenal);
+    }
+
+    public void addListenerOnButton() {
+        Button buttonPersonSave = findViewById(R.id.person_save);
+        Button buttonPersonCancel = findViewById(R.id.person_cancel);
+//        final String personFIO = ((EditText)findViewById(R.id.person_fio)).getText().toString();
+//        final String personNickname = ((EditText)findViewById(R.id.nickname)).getText().toString();
+//        final String personPosition = ((EditText)findViewById(R.id.person_position)).getText().toString();
+//        final String personArsenal = ((EditText)findViewById(R.id.arsenal)).getText().toString();
 
 //        FirebaseDatabase database = FirebaseDatabase.getInstance();
 //        DatabaseReference db_personFIO;
@@ -47,32 +74,15 @@ public class PersonActivity extends AppCompatActivity {
 //        db_personArsenal.setValue(personArsenal);
 
 
+
         buttonPersonSave.setOnClickListener(
                 new View.OnClickListener() {
 
 
                     @Override
                     public void onClick(View view) {
-                        FirebaseDatabase database = FirebaseDatabase.getInstance();
-                        DatabaseReference db_personFIO;
-                        DatabaseReference db_personNickname;
-                        DatabaseReference db_personPosition;
-                        DatabaseReference db_personArsenal;
-                        db_personFIO = database.getReference("Team/members_id/1/FIO");
-                        db_personNickname= database.getReference("Team/members_id/1/Nickname");
-                        db_personPosition = database.getReference("Team/members_id/1/Position");
-                        db_personArsenal = database.getReference("Team/members_id/1/Arsenal");
-                        db_personFIO = database.getReference("Team/members_id/1/FIO");
-                        db_personNickname= database.getReference("Team/members_id/1/Nickname");
-                        db_personPosition = database.getReference("Team/members_id/1/Position");
-                        db_personArsenal = database.getReference("Team/members_id/1/Arsenal");
-
-                        db_personFIO.setValue(personFIO);
-                        db_personNickname.setValue(personNickname);
-                        db_personPosition.setValue(personPosition);
-                        db_personArsenal.setValue(personArsenal);
-                        Intent i = new Intent(".MembersActivity");
-                        startActivity(i);
+                        to_db();
+                        finish();
                     }
                 }
 
@@ -81,8 +91,7 @@ public class PersonActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Intent i = new Intent(".MembersActivity");
-                        startActivity(i);
+                        finish();
                     }
                 }
 
