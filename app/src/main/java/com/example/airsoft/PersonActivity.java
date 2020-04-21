1e290e6291c9972695bb7085727835c655acc7c1
AnnaPerekrestova <anna.perekrestova111@gmail.com>
2020-04-21 20:53:54 +0300

    new push with db

--- app/src/main/java/com/example/airsoft/PersonActivity.java
+++ app/src/main/java/com/example/airsoft/PersonActivity.java
@@ -7,6 +7,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.w3c.dom.Text;
 
@@ -18,24 +22,55 @@ public class PersonActivity extends AppCompatActivity {
         setContentView(R.layout.activity_person);
         addListenerOnButton();
     }
-    @SuppressLint("WrongViewCast")
+
     public void addListenerOnButton() {
         Button buttonPersonSave = findViewById(R.id.person_save);
         Button buttonPersonCancel = findViewById(R.id.person_cancel);
-        //Text personFIO = findViewById(R.id.person_fio)();
-        //Text personFIO = findViewById(R.id.person_fio);
-        
-        
-        
-        //Button buttonMembers = findViewById(R.id.members);
+        final String personFIO = ((EditText)findViewById(R.id.person_fio)).getText().toString();
+        final String personNickname = ((EditText)findViewById(R.id.nickname)).getText().toString();
+        final String personPosition = ((EditText)findViewById(R.id.person_position)).getText().toString();
+        final String personArsenal = ((EditText)findViewById(R.id.arsenal)).getText().toString();
+
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference db_personFIO;
+//        DatabaseReference db_personNickname;
+//        DatabaseReference db_personPosition;
+//        DatabaseReference db_personArsenal;
+//        db_personFIO = database.getReference("Team/members_id/1/FIO");
+//        db_personNickname= database.getReference("Team/members_id/1/Nickname");
+//        db_personPosition = database.getReference("Team/members_id/1/Position");
+//        db_personArsenal = database.getReference("Team/members_id/1/Arsenal");
+
+//        db_personFIO.setValue(personFIO);
+//        db_personNickname.setValue(personNickname);
+//        db_personPosition.setValue(personPosition);
+//        db_personArsenal.setValue(personArsenal);
+
+
         buttonPersonSave.setOnClickListener(
                 new View.OnClickListener() {
-
-                    
 
 
                     @Override
                     public void onClick(View view) {
+                        FirebaseDatabase database = FirebaseDatabase.getInstance();
+                        DatabaseReference db_personFIO;
+                        DatabaseReference db_personNickname;
+                        DatabaseReference db_personPosition;
+                        DatabaseReference db_personArsenal;
+                        db_personFIO = database.getReference("Team/members_id/1/FIO");
+                        db_personNickname= database.getReference("Team/members_id/1/Nickname");
+                        db_personPosition = database.getReference("Team/members_id/1/Position");
+                        db_personArsenal = database.getReference("Team/members_id/1/Arsenal");
+                        db_personFIO = database.getReference("Team/members_id/1/FIO");
+                        db_personNickname= database.getReference("Team/members_id/1/Nickname");
+                        db_personPosition = database.getReference("Team/members_id/1/Position");
+                        db_personArsenal = database.getReference("Team/members_id/1/Arsenal");
+
+                        db_personFIO.setValue(personFIO);
+                        db_personNickname.setValue(personNickname);
+                        db_personPosition.setValue(personPosition);
+                        db_personArsenal.setValue(personArsenal);
                         Intent i = new Intent(".MembersActivity");
                         startActivity(i);
                     }
