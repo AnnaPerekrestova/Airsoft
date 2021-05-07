8d3c007467fde39ea487dd7f94e111987e8122e2
AnnaPerekrestova <anna.perekrestova111@gmail.com>
2021-05-07 05:33:15 +0300

    создание игр и вывод запалнированных игр

--- app/src/main/java/com/example/airsoft/Activities/GameInfoActivity.java
+++ app/src/main/java/com/example/airsoft/Activities/GameInfoActivity.java
@@ -11,95 +11,104 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
-
-import com.example.airsoft.Adapters.MemberTeamGameAdapter;
-import com.example.airsoft.Classes.MemberTeamClass;
 import com.example.airsoft.R;
-import com.example.airsoft.RecyclerViewDecorator;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
+import com.example.data.FirebaseData;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class GameInfoActivity extends AppCompatActivity {
-    private RecyclerView rv;
-    Intent intent;
-    String member_team_id;
-    String game_id;
-    String used_teams_string;
-    public MemberTeamGameAdapter adapter;
-    public List<MemberTeamClass> member_team_list =new  ArrayList<>();
-    List<List<String>> list_of_teams = new ArrayList<>();
+
+    FirebaseData fbData = new FirebaseData().getInstance();
+    String GameID;
+    String PolygonID;
+    String OrgcomID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_info);
-        rv = findViewById(R.id.game_info_teams);
-        intent = getIntent();
-        //-----Получаем значения переданные через intent----------------------------------------------------------------
-        game_id= intent.getStringExtra("game_id");
-        member_team_id = intent.getStringExtra("member_team_id");
-        used_teams_string = intent.getStringExtra("used_teams");
-//--------Заполняем текст вью соответствующими значениями---------------------------------------------------------------
-        String datetime = intent.getStringExtra("date_time");
-        TextView dt =findViewById(R.id.GameDataTime);
-        dt.setText(datetime);
-        String map = intent.getStringExtra("map");
-        TextView m =findViewById(R.id.game_map);
-        m.setText(map);
-        String winner = intent.getStringExtra("winner");
-        TextView w =findViewById(R.id.game_winner);
-        w.setText(winner);
-//------Добавляем разделение между строками в RecyclerView ------------------------------------------------------
-        rv.addItemDecoration(new RecyclerViewDecorator(this, LinearLayoutManager.VERTICAL, 16));
 
-//-------Создаем адаптер для RecyclerView вида GridLayout с двумя колонками (list_of_teams заполняется в функции GetMembersOfUsedTeams)---------------------------------------------------------------------------
-        adapter = new MemberTeamGameAdapter(list_of_teams);
-        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 2, LinearLayoutManager.VERTICAL,false);;
-        rv.setLayoutManager(mLayoutManager);
-        rv.setItemAnimator(new DefaultItemAnimator());
-        rv.setAdapter(adapter);
-//------Заполняем список списков строк (распределение по командам) для адаптера ---------------------------------------------
-        GetMembersOfUsedTeams(member_team_id);
-    }
-//----Получаем список списков строк для получения информации о том, кто в какой команде-------------------------------------------
-    private void GetMembersOfUsedTeams(String member_team_id) {
-        final DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference("MembersTeams");
-        databaseRef.child("id").child(member_team_id).addListenerForSingleValueEvent(new ValueEventListener() {
+        Intent intent = getIntent();
+        GameID = intent.getStringExtra("gameID");
+
+
+        Spinner spinnerStatuses = findViewById(R.id.game_status_spinner);
+        String[] statusesList= {"открыт набор на игру","набор на игру закрыт","игра отменена","игра идет","игра прошла"};
+        ArrayAdapter<String> adapterStatuses = new ArrayAdapter<String>(GameInfoActivity.this, android.R.layout.simple_spinner_item, statusesList);
+        adapterStatuses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerStatuses.setAdapter(adapterStatuses);
+        spinnerStatuses.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot == null) return;
-                for (DataSnapshot postSnapShot : dataSnapshot.getChildren()) {
-                    //String team = postSnapShot.getKey();
-                    String members = postSnapShot.getValue().toString();
-                    String[] s =members.split("[,\\]\\[]");
-                    List<String> list = new ArrayList<>();
-                    for (String i: s){
-                        if (!i.equals("")){
-                            list.add(i);
-                        }
-                    }
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // Получаем выбранный объект
+                String selectedStatus = (String) parent.getItemAtPosition(position);
 
-                    list_of_teams.add(list);
-                }
-                adapter.notifyDataSetChanged();
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
-                // Error
-                Log.d("Error", "databaseError");
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
+        addListenerOnButton();
+        getData();
 
     }
 
+    public void getData(){
+        fbData.getGameInfo(new FirebaseData.gameInfoCallback() {
+            @Override
+            public void onGameInfoChanged(String orgcomID, String gameName, String gameDate, String polygonID, String gameStatus, String gameDescription, String gameWinner, String gameSides) {
+                fillGameInfo(gameName, gameDate, gameDescription);
+                PolygonID = polygonID;
+                OrgcomID = orgcomID;
+            }
+        },GameID);
+    }
 
+    public void addListenerOnButton() {
+        Button buttonTeams = findViewById(R.id.Game_players_btn);
+        Button buttonPolygon = findViewById(R.id.Game_polygon_btn);
+
+
+        buttonTeams.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent i = new Intent(".Activities.PlayingTeamsActivity");
+                        i.putExtra("gameID", GameID);
+                        startActivity(i);
+                    }
+                }
+
+        );
+        buttonPolygon.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent i = new Intent(".PolygonInfoActivity");
+                        i.putExtra("polygonID", PolygonID);
+                        startActivity(i);
+                    }
+                }
+
+        );
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void fillGameInfo(String name, String date, String descr){
+        TextView gname = (TextView) findViewById(R.id.game_info_name);
+        TextView gdate = (TextView) findViewById(R.id.GameDataTime);
+        TextView gdescr = (TextView) findViewById(R.id.Game_description);
+
+        gname.setText(name);
+        gdate.setText(date);
+        gdescr.setText(descr);
+    }
 }
